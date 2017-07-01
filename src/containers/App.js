606b08a90b860ba2cc71fb0467affc22c0827116
Mikhail Metrikin <metrikin@gmail.com>
2017-07-01 14:50:55 -0700

    Refactoring with Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import GifsTemp from '../Components/GifsTemp';
 import { connect } from 'react-redux';
 
+import { bindActionCreators } from 'redux';
+import * as Actions from '../actions';
+import SearchBar from '../Components/SearchBar';
+import '../style/app.css';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <GifsTemp gifs={this.props.gifs} />
+        <SearchBar onTermChange={this.props.actions.requestGifs} />
       </div>
     );
   }
@@ -18,4 +23,10 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(Actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
