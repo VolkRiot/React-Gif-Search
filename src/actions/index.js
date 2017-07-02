56413217485df0c48c897394baa78f39f40992ed
Mikhail Metrikin <metrikin@gmail.com>
2017-07-01 20:19:19 -0700

    Modal Reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,9 @@ const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = '&api_key=64031ee213914402ad2a1d49b11c3b79';
 
 export const REQUEST_GIFS = "REQUEST_GIFS";
+export const OPEN_MODAL = "OPEN_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+
 
 export function requestGifs(term = null) {
   const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
@@ -11,5 +14,18 @@ export function requestGifs(term = null) {
   return {
     type: REQUEST_GIFS,
     payload: data,
+  }
+}
+
+export function openModal(gif) {
+  return {
+    type: OPEN_MODAL,
+    gif
+  }
+}
+
+export function closeModal() {
+  return {
+    type: CLOSE_MODAL,
   }
 }
