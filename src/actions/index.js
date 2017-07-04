fa223874a77bffa115e7d0f4332c30f1cda40462
Mikhail Metrikin <metrikin@gmail.com>
2017-07-04 14:06:54 -0700

    Auth paths connected with actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,29 +3,42 @@ import request from 'superagent';
 const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = '&api_key=64031ee213914402ad2a1d49b11c3b79';
 
-export const REQUEST_GIFS = "REQUEST_GIFS";
-export const OPEN_MODAL = "OPEN_MODAL";
-export const CLOSE_MODAL = "CLOSE_MODAL";
-
+export const REQUEST_GIFS = 'REQUEST_GIFS';
+export const OPEN_MODAL = 'OPEN_MODAL';
+export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const SIGN_IN_USER = 'SIGN_IN_USER';
+export const SIGN_OUT_USER = 'SIGN_OUT_USER';
 
 export function requestGifs(term = null) {
   const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
 
   return {
     type: REQUEST_GIFS,
-    payload: data,
-  }
+    payload: data
+  };
 }
 
 export function openModal(gif) {
   return {
     type: OPEN_MODAL,
     gif
-  }
+  };
 }
 
 export function closeModal() {
   return {
-    type: CLOSE_MODAL,
-  }
+    type: CLOSE_MODAL
+  };
+}
+
+export function signInUser() {
+  return {
+    type: SIGN_IN_USER
+  };
+}
+
+export function signOutUser() {
+  return {
+    type: SIGN_OUT_USER
+  };
 }
